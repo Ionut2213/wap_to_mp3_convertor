7247f31f0040a54cdcc747d42cff53d34f395ec8
laote <ihagiu1212@gmail.com>
2024-12-18 10:32:34 +0200

    What we did until now: we set the env,we did the imports part and we install the module called pydub

---
+++ main.py
@@ -0,0 +1,8 @@
+# What imports we need and what libraries are we gonna use
+import os
+from tkinter import Tk, Label, Button, filedialog, StringVar, Entry
+# if you don t have tkinter installed use the command pip install tkinter
+
+# Another library that we gonne use for audio files is pydub
+# Command to install it pip install pydub
+from pydub import AudioSegment
